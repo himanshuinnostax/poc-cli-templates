33a669bdea801c3509750a6a5ad3a3729b30c6b3
HimanshuGautamVs <himanshu.gautam@vicesoftware.com>
2021-08-19 13:25:08 +0530

    node option with index.js and package.json

--- src/index.js
+++ src/index.js
@@ -13,37 +13,49 @@ const CHOICES = fs.readdirSync(path.join(__dirname, "templates"));
 
 const QUESTIONS = [
   {
-    name: "project-choice",
+    name: "projectChoice",
     type: "list",
     message: "What project template would you like to generate?",
-    choices: CHOICES
+    choices: CHOICES,
   },
   {
     name: "project-name",
     type: "input",
     message: "Project name:",
-    validate: function(input) {
+    validate: function (input) {
       if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
       else
         return "Project name may only include letters, numbers, underscores and hashes.";
-    }
-  }
+    },
+  },
+  {
+    name: "default-route",
+    type: "input",
+    message: "Enter the default route",
+    when: (answers) => {
+      return answers.projectChoice === "node";
+    },
+  },
 ];
 
-inquirer.prompt(QUESTIONS).then(answers => {
-  const projectChoice = answers["project-choice"];
+inquirer.prompt(QUESTIONS).then((answers) => {
+  const projectChoice = answers["projectChoice"];
   const projectName = answers["project-name"];
   const templatePath = path.join(__dirname, "templates", projectChoice);
-
+  const defaultRoute = answers["default-route"];
   fs.mkdirSync(`${CURR_DIR}/${projectName}`);
 
-  createDirectoryContents(templatePath, projectName);
+  createDirectoryContents(templatePath, projectName, defaultRoute);
 });
 
-function createDirectoryContents(templatePath, newProjectPath) {
+function createDirectoryContents(
+  templatePath,
+  newProjectPath,
+  newDefaultRoute
+) {
   const filesToCreate = fs.readdirSync(templatePath);
 
-  filesToCreate.forEach(file => {
+  filesToCreate.forEach((file) => {
     const origFilePath = `${templatePath}/${file}`;
 
     // get stats about the current file
@@ -51,7 +63,10 @@ function createDirectoryContents(templatePath, newProjectPath) {
 
     if (stats.isFile()) {
       let contents = fs.readFileSync(origFilePath, "utf8");
-      contents = render(contents, { projectName: newProjectPath });
+      contents = render(contents, {
+        projectName: newProjectPath,
+        defaultRoute: newDefaultRoute,
+      });
 
       const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
       fs.writeFileSync(writePath, contents, "utf8");
